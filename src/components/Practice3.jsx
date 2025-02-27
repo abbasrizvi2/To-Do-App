3800f6f253836931b6fd5ce3b982cd9002c9dd4f
abbasrizvi2 <abbasali0974@gmail.com>
2025-02-28 00:30:17 +0530

    done with basic add remove done task

--- src/components/Practice3.jsx
+++ src/components/Practice3.jsx
@@ -1,8 +1,74 @@
+import { useState } from "react"
+import "./App.css"
+export function Practice3() {
+    const [item, setItem] = useState("")
+    const [task, setTask] = useState([])
 
-export default function Practice3() {
+    const handleAdd = ()=>{
+        const newTask = task.map((prev) => {
+            return {...prev} 
+        })
+        newTask.push({value:item , isDone:false , id:new Date().getTime()})
+        setTask(newTask)
+        setItem("")
+    }
+
+    const handleEnter = (e) => {
+        if (e.key === "Enter") {
+            return handleAdd()
+        }
+    }
+
+    const doneTask = (id) => {
+        const done = task.map((data) => {
+            if (data.id === id) {
+                data.isDone = !data.isDone
+                return data
+            }
+        })
+       setTask([...task,done])
+    }
+
+    const deleteTask = (id) => {
+        const deletes = task.filter((data) => {
+            if (data.id !== id) {
+            return data
+            }
+        })
+        setTask(deletes)
+    }
+
     return (
-        <div>
-            Hmm
+        <div className="font-bold w-6/12 mx-auto  p-10 text-center h-screen">
+            <div>
+                <span>
+                    <input type="text"
+                        value={item}
+                        onChange={(e) => setItem(e.target.value)}
+                        onKeyDown={handleEnter}
+                        className="border p-2" />
+                </span>
+                <span><button className="border p-3 mx-2" onClick={handleAdd}>Add Task</button></span>
+            </div>
+            <div className="w-[400px] mx-40 mt-2.5 text-2xl ">
+                {task.map((data) => {
+                return (
+                    <div key={data.id} className="flex justify-between items-center ">
+                        {data.isDone ? <div>
+                            <span className="line-through">{data.value}</span>
+                        </div>
+                            : <div>
+                                <span>{data.value}</span>
+                            </div>}
+                        
+                        <div>
+                        <span onClick={()=>doneTask(data.id)}>✔️</span>
+                            <span onClick={()=>deleteTask(data.id)}>⛔</span>
+                            </div>
+                    </div>
+                )
+                })}
+            </div>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 // import App from './components/App'
 // import Practice from './components/Practice'
 // import {Practice2} from './components/Practice2'
-import {Practice3} from './components/Practice2'
+import {Practice3} from './components/Practice3'
 
 createRoot(document.getElementById('root')).render(
 
